29d2f366aaabb6bad32118f0b0003680425b87b8
nkayilovette03 <nkayilovette03@gmail.com>
2025-01-10 01:39:28 -0500

    Change Username validation in webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
       const mongoUser = await createUser({
         clerkId: id,
         name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-        username: username!,
+        username: username! !== null ? username! : "",
         email: email_addresses[0].email_address,
         picture: image_url,
       });
@@ -84,7 +84,7 @@ export async function POST(req: Request) {
       clerkId: id,
       updateData: {
         name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-        username: username!,
+        username: username! !== null ? username! : "",
         email: email_addresses[0].email_address,
         picture: image_url,
       },
